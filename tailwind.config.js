7e436b01fa8e12d339123f68c18c6ea2445a5b29
parthgabani777 <49564563+parthgabani777@users.noreply.github.com>
2022-08-05 17:27:28 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./dist/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#23b7a4",
+                secondary: "#3a454d",
+                header: "#1d2126",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
