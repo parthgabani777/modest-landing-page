e753ec9a80f51c4d087f791120767a3913637ba4
parthgabani777 <49564563+parthgabani777@users.noreply.github.com>
2022-08-05 20:13:13 +0530

    feat: Added font and container breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
                 secondary: "#3a454d",
                 header: "#1d2126",
             },
+            container: {
+                screens: {
+                    sm: "600px",
+                    md: "728px",
+                    lg: "984px",
+                    xl: "1240px",
+                    "2xl": "1140px",
+                },
+            },
+            fontFamily: {
+                "open-sens": "Open Sans",
+            },
         },
     },
     variants: {
